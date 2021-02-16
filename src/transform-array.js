24f98cd82fc101918dedd24e0b96560cded1f49e
Alexander <alexanchek@sashkin-blog.ru>
2021-02-16 20:02:50 +0300

    init: 4 transform-array  tests completed

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,39 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function transform(arr) {
+  arrCopy=[...arr]
+
+  for (let i = 0; i < arrCopy.length; i++) {
+    keywords = arrCopy[i];
+
+    switch (true) {
+      case keywords === '--discard-next' && arrCopy[i + 1] != undefined:
+        arrCopy.splice(i, 2);
+        break;
+      case keywords === '--discard-prev' && arrCopy[i - 1] != undefined:
+        arrCopy.splice(i - 1, 2);
+        break;
+      case keywords === '--double-next' && arrCopy[i + 1] != undefined:
+        arrCopy.splice(i, 1, arrCopy[i + 1]);
+        break;
+      case keywords === '--double-prev' && arrCopy[i - 1] != undefined:
+        arrCopy.splice(i, 1, arrCopy[i + 1]);
+        break;
+      case keywords === '--discard-next' && arrCopy[i + 1] === undefined:
+        arrCopy.splice(i, 1);
+        break;
+      case keywords === '--discard-prev' && arrCopy[i - 1] === undefined:
+        arrCopy.splice(i, 1);
+        break;
+      case keywords === '--double-next' && arrCopy[i + 1] === undefined:
+        arrCopy.splice(i, 1);
+        break;
+      case keywords === '--double-prev' && arrCopy[i - 1] === undefined:
+        arrCopy.splice(i, 1);
+        break;
+    }
+  }
+
+  return arrCopy;
+  
+}
