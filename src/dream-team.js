3e61aca56a1f0c984283a0a51e9a214ca1ec3612
Alexander <alexanchek@sashkin-blog.ru>
2021-02-16 18:25:24 +0300

    init: all tests passed

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,25 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members = []) {
+  if (members === null) {
+    return false;
+  }
+
+  let checkElement = "";
+  let names = "";
+
+  for (let i = 0; i < members.length; i++) {
+    checkElement = members[i];
+
+    switch (typeof checkElement) {
+      case "string":
+        if (checkElement.split(" ").length > 2) {
+          checkElement = checkElement.split(" ").join("");
+        }
+        names += checkElement.charAt(0).toUpperCase();
+    }
+  }
+
+  names = names.split("").sort().join("");
+  return names;
 };
