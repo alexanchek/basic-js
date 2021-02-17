d083f3b234cb1d23dc9d19a691899c69fc4a50d5
Alexander <alexanchek@sashkin-blog.ru>
2021-02-17 18:25:39 +0300

    init: hanoi tower tests passed

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,6 +1,14 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function calculateHanoi(/* disksNumber, turnsSpeed */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function calculateHanoi(disksNumber, turnsSpeed) {
+  turns = Math.pow(2, disksNumber) - 1;
+
+  speed = turnsSpeed / 3600;
+
+  seconds = Math.floor(turns / speed);
+
+  return (answer = {
+    turns: this.turns,
+    seconds: this.seconds,
+  });
 };
--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,7 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(arr) {
+module.exports =function transform(arr) {
+
   arrCopy=[...arr]
 
   for (let i = 0; i < arrCopy.length; i++) {
@@ -17,12 +18,14 @@ module.exports = function transform(arr) {
         arrCopy.splice(i, 1, arrCopy[i + 1]);
         break;
       case keywords === '--double-prev' && arrCopy[i - 1] != undefined:
-        arrCopy.splice(i, 1, arrCopy[i + 1]);
+        arrCopy.splice(i, 1, arrCopy[i - 1]);
         break;
-      case keywords === '--discard-next' && arrCopy[i + 1] === undefined:
         arrCopy.splice(i, 1);
         break;
       case keywords === '--discard-prev' && arrCopy[i - 1] === undefined:
+        arrCopy.splice(i, 1);
+        break;
+        case keywords === '--discard-next' && arrCopy[i + 1] === undefined:
         arrCopy.splice(i, 1);
         break;
       case keywords === '--double-next' && arrCopy[i + 1] === undefined:
