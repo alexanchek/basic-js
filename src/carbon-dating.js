5db4c41716d0254b5a0a656a762c1d16ce33214a
Alexander <alexanchek@sashkin-blog.ru>
2021-02-23 13:18:59 +0300

    fix: 5 carbon-dating tests passed

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,9 +1,18 @@
 const CustomError = require("../extensions/custom-error");
 
-const MODERN_ACTIVITY= 15; 
-const HALF_LIFE_PERIOD= 5730;
+const MODERN_ACTIVITY = 15;
+const HALF_LIFE_PERIOD = 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+
+  if (typeof sampleActivity != "number" || sampleActivity === 'undefined' ||
+  sampleActivity === 'null' || Array.isArray(sampleActivity)) {
+    return false;
+  }
+
+  floatActivity = parseFloat(sampleActivity);
+
+  let k = 0.693 / HALF_LIFE_PERIOD;
+  let t = Math.ceil(Math.log2(MODERN_ACTIVITY / floatActivity) / k);
+  return t;
 };
