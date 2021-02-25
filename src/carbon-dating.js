bec738ed12d02e40959b9a2c7d0c109f49a71c96
Alexander <alexanchek@sashkin-blog.ru>
2021-02-25 18:13:12 +0300

    fix: all carbon-dating tests passed

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -5,13 +5,12 @@ const HALF_LIFE_PERIOD = 5730;
 
 module.exports = function dateSample(sampleActivity) {
 
-  if (typeof sampleActivity != "number" || sampleActivity === 'undefined' ||
-  sampleActivity === 'null' || Array.isArray(sampleActivity)) {
+  if (typeof sampleActivity != 'number' || sampleActivity != 'string' )  {
     return false;
   }
 
   floatActivity = parseFloat(sampleActivity);
-
+  
   let k = 0.693 / HALF_LIFE_PERIOD;
   let t = Math.ceil(Math.log2(MODERN_ACTIVITY / floatActivity) / k);
   return t;
