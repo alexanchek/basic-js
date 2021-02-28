309a94fe376adc07a5f9aca6190b6c08262139c3
Alexander <alexanchek@sashkin-blog.ru>
2021-02-28 11:27:07 +0300

    feat: added reverse cases

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -2,42 +2,52 @@ const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
   constructor(value) {
-    this.value = value;
+    if (value === undefined) {
+      this.value = true;
+    } else {
+      this.value = value;
+    }
   }
 
   encrypt(message, key) {
+
     if (!message || !key) {
-      throw new Error("Something went bad :( Please, check your parameters");
+      throw new Error("Something went bad :( Please, check your parameters")
     }
-
-    // prepare arrays
-    message = message.toLowerCase().split("");
-    key = key.toLowerCase().split("");
-
-    let alphabet = "abcdefghijklmnopqrstuvwxyz";
-    let resultLine = "";
-
-    // every step we add a letter for the future steps.
+  
+    // prepare arrays 
+    message = message.toLowerCase().split('');
+    key = key.toLowerCase().split('');
+  
+    let alphabet = 'abcdefghijklmnopqrstuvwxyz';
+    let resultLine = [];
+  
+    // every step we add a letter for the future steps. 
     //If it's a letter we will calculate its new position, otherwise we add a space to keys array
     for (let i = 0; i < message.length; i++) {
       if (message[i].charCodeAt() >= 97 && message[i].charCodeAt() <= 122) {
-        key.push(key[i]);
-
-        let formulaVignere =
-          alphabet.indexOf(message[i]) + (alphabet.indexOf(key[i]) % 26);
+        key.push(key[i])
+  
+  
+        let formulaVignere = alphabet.indexOf(message[i]) + alphabet.indexOf(key[i]) % 26;
         if (formulaVignere < 25) {
-          resultLine += alphabet[formulaVignere];
+          resultLine.push(alphabet[formulaVignere]);
         } else {
-          resultLine += alphabet[formulaVignere - 26];
+          resultLine.push(alphabet[formulaVignere - 26]);
         }
       } else {
-        resultLine += String.fromCharCode(message[i].charCodeAt());
-        key.splice(i, 0, " ");
+        resultLine.push(String.fromCharCode(message[i].charCodeAt()));
+        key.splice(i, 0, ' ');
       }
     }
+    if (this.value) {
+      return resultLine.join('').toUpperCase();
+    } else {
+      return resultLine.reverse().join('').toUpperCase();
+    }
 
-    return resultLine.toUpperCase();
   }
+  
 
   decrypt(message, key) {
     if (!message || !key) {
